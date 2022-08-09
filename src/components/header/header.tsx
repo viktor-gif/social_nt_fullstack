519c93cfa690a9ec24c6d19a9571f85b6e45e0b4
viktor-gif <klim.victor.ua@gmail.com>
2022-08-09 18:42:21 +0300

    Menu in header styling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,20 +5,28 @@ import logo from "../../img/logo.png"
 import { NavLink } from "react-router-dom"
 import { AuthDataType } from "../../ts/auth"
 import { logout } from "../../redux/authReducer"
-import { Navigate } from "react-router-dom"
+import { Button } from "../common/button/Button"
+import avatar from "../../img/ava_male.jpeg"
+import { ProfileDataType } from "../../ts/profile"
+import { LogOut } from "react-ionicons"
+import { useState } from "react"
 
 type PropsType = {
     authData: AuthDataType | null
     isAuth: boolean
+    authProfileData: ProfileDataType | null
 
     logout: () => void
 }
 const Header = (props: PropsType) => {
+    const [isMenuActionsActive, setmenuActionsActive] = useState(false)
     
     const logoutClick = () => {
         props.logout()
     }
-    
+    const toggleMenuActions = () => {
+        isMenuActionsActive ? setmenuActionsActive(false) : setmenuActionsActive(true)
+    }
     
     return <div className={s.header}>
         <img src={logo} className={s.header__logo} />
@@ -26,15 +34,27 @@ const Header = (props: PropsType) => {
             <NavLink to="/user/add" className={s.header__actions_item}>Створити нового користувача</NavLink>
             <NavLink to="/login" className={s.header__actions_item}>Зайти на свою сторінку</NavLink>
         </div>
-        : <button onClick={logoutClick}>
-            Вийти з свого профілю
-            </button>}
+            :
+        <div className={s.menuProfileActions} onClick={toggleMenuActions}>
+            <img src={props.authProfileData?.photos.small || avatar} alt="ava" />
+            {isMenuActionsActive
+                
+            && <ul className={s.actionsList}>
+                <li onClick={logoutClick} >
+                    <LogOut color={'#555'}/> 
+                    <span>Вийти з свого профілю</span>
+                </li>
+            </ul> 
+            }
+        </div>
+        }
     </div>
 }
 
 const mapStateToProps = (state: AppStateType) => ({
     authData: state.auth.authData,
-    isAuth: state.auth.isAuth
+    isAuth: state.auth.isAuth,
+    authProfileData: state.auth.authProfileData
 })
 
 export default connect(mapStateToProps, {
