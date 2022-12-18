b3f9b4488a13aeaaf4546cc51496bae2f2316690
viktor-gif <klim.victor.ua@gmail.com>
2022-12-18 14:26:36 +0200

    Groop info styling

--- src/components/ groopInfo/groopInfo.tsx
+++ src/components/ groopInfo/groopInfo.tsx
@@ -4,8 +4,13 @@ import { useParams } from "react-router"
 import { AppStateType } from "../../redux/redux-store"
 import { getGroopInfo } from "../../redux/groopsReducer"
 import { GroopType } from "../../ts/groops"
+import s from './groopInfo.module.css'
+import avatar from '../../img/ava_groops.png'
+import { AuthDataType } from "../../ts/auth"
+import { Button } from "../common/button/Button"
 
 type PropsType = {
+    authData: AuthDataType | null
     groopInfo: GroopType | null
     getGroopInfo: (groopId: string) => void
 }
@@ -20,11 +25,44 @@ const GroopInfo = (props: PropsType) => {
         params.groopId && props.getGroopInfo(params.groopId)
     }, [])
 
-    return <div>{props.groopInfo && props.groopInfo._id + props.groopInfo.title}</div>
+    const isFollower = (props.groopInfo && props.authData) && props.groopInfo.followers.includes(props.authData.id)
+
+    return <div className={s.groopInfo}>
+        <div className={s.header}>
+            <div className={s.groop__image}>
+                <img src={props.groopInfo?.mainImg || avatar} alt="Groop" />
+            </div>
+            <div className={s.groop__info}>
+                <h3 className={s.groop__title}>
+                    {props.groopInfo?.title}
+                </h3>
+                <div className={s.groop__isFollower}>
+                    {isFollower
+                        ? 'Ви підписані'
+                        : props.groopInfo?.followers.length + ' підписників'
+                    }
+                </div>
+
+                <button className={s.followGroop}
+                    onClick={() => console.log('test')}    
+                >
+                    {isFollower ? 'Відписатись' : 'Підписатись'}
+                </button>
+                
+            </div>
+        </div>
+        <div className={s.posts}>
+            POSTS
+        </div>
+        <div className={s.sidebar}>
+            SIDEBAR
+        </div>
+    </div>
 }
 
 const mapStateToProps = (state: AppStateType) => ({
-    groopInfo: state.groops.groopInfo
+    groopInfo: state.groops.groopInfo,
+    authData: state.auth.authData
 })
 
 export default connect(mapStateToProps, {
